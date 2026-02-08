695576ab6d4d96521b1f9ae111408ba5bb7a023f
tanimoto <tanimoto.masashi@protonmail.com>
2026-02-08 11:02:19 +0900

    update

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -1,18 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "mah";
-  home.homeDirectory = "/Users/mah";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "25.11"; # Please read the comment before changing.
 
   # nixpkgs の設定
@@ -190,6 +178,8 @@
 
       # その他の補完設定
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'  # 大文字小文字を区別しない
+
+      source ${./zsh-functions.zsh}
     '';
   };
 }
