95c6fdf056868c4caec78013bc2df79546e647e3
tnmt <masashi.tanimoto@beproud.jp>
2026-02-23 08:50:26 +0900

    update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -14,14 +14,19 @@ local config = wezterm.config_builder()
 config.font_size = 16
 config.color_scheme = 'Catppuccin Mocha'
 
+---------------------------
 -- my custom
+---------------------------
 --config.font = wezterm.font("FirgeNerd Console", { weight = 'Regular' })
-config.font = wezterm.font_with_fallback { 'JetBrains Mono', '源柔ゴシックL' }
+config.font = wezterm.font_with_fallback { 'JetBrains Mono', '源柔ゴシックL', 'FirgeNerd Console' }
 config.use_ime = true
 config.automatically_reload_config = true
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
--- config.default_prog = { '/bin/zsh', '--login', '-c', 'exec /opt/homebrew/bin/nu' }
+
+-- 左右どちらの Option も「記号合成せず、普通の Alt(meta)」として扱いたい場合
+config.send_composed_key_when_left_alt_is_pressed  = false
+config.send_composed_key_when_right_alt_is_pressed = false
 
 -- Finally, return the configuration to wezterm:
 return config
