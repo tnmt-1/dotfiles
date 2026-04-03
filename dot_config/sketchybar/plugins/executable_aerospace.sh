2d07c2d3b442840c2f89626cc141a454ebef2e41
tnmt-1 <tanimioto.masashi@protonmail.com>
2026-04-04 08:08:14 +0900

    update

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+WORKSPACE_ID="$1"
+NAME="space.$WORKSPACE_ID"
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+if [ "$WORKSPACE_ID" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME background.drawing=on
 else
     sketchybar --set $NAME background.drawing=off
