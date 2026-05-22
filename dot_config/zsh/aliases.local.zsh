35fbe3af8a4ae7f39e327bc6a620bef6dc57a8d0
tnmt-1 <tanimioto.masashi@protonmail.com>
2026-05-23 08:24:51 +0900

    update

--- dot_config/zsh/aliases.local.zsh
+++
@@ -1,4 +0,0 @@
-# 端末固有の alias を管理する
-alias kabep='ssh kabe -D localhost:1080 -N -C tnmt@kabe.beproud.jp'
-alias spectacular='cd src && poetry run python manage.py makemessages -l ja && poetry run python manage.py makemessages -l en && poetry run python manage.py compilemessages && poetry run python manage.py spectacular --file ../schema.yml && cd ../'
-alias upup="brew update && brew upgrade && brew autoremove && mise up"
